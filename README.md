9125ee3facd5c37b6b9279b1f1c88491b5721b17
mxamin <amin.solhizadeh@gmail.com>
2015-04-30 18:39:09 +0430

    Initial Presentation

---
+++ README.md
@@ -0,0 +1,4 @@
+Test Driven Development with Flask
+==================================
+
+This is the presentation file of my speech at Iran PyCon 2015.
---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Test Driven Development with Flask',
+    //subtitle: '',
+    eventInfo: {
+      title: 'Iran PyCon',
+      date: 'May 1, 2015'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/flask_logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Mohammad Amin Solhizadeh',
+    //company: 'Iran PyCon 2015',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@mxamin',
+    www: 'mxamin.com',
+    github: 'http://github.com/mxamin'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
